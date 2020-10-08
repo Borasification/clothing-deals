23849ba87ea2ce4bb952dd3239838bdc2e54071e
Maël Lavault <mael.lavault@shopify.com>
2020-10-08 19:00:24 -0400

    Remove unused routes

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-require_dependency "clothing_deals_constraint"
-
-ClothingDeal::Engine.routes.draw do
-  get "/" => "clothing_deals#index", constraints: ClothingDealConstraint.new
-  get "/actions" => "actions#index", constraints: ClothingDealConstraint.new
-  get "/actions/:id" => "actions#show", constraints: ClothingDealConstraint.new
-end
