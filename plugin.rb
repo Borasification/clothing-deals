d99e4a52cc822469eba18f06b8d0c0d728e8d3fd
Maël Lavault <mael.lavault@shopify.com>
2020-10-11 21:43:10 -0400

    WIP

--- plugin.rb
+++ plugin.rb
@@ -93,8 +93,82 @@ after_initialize do
     end
   end
 
-  DiscourseEvent.on(:post_created) do |*params|
-    post, opts, user = params
+  def on_post_edited(original_post, modified_post)
+    categories_to_fields = {
+      "veste" => "size_top",
+      "chemise" => "size_shirt",
+      "pantalon" => "size_bottom",
+      "chaussures" => "size_shoes",
+      "gants" => "size_gloves",
+      "chapeau" => "size_hat"
+    }
+    # original_post_contents = original_post.cached_original_raw.to_s
+    original_post_contents = original_post.original_raw.to_s
+    modified_post_contents = modified_post.raw.to_s
+
+    bot_username = SiteSetting.deal_bot_user
+    user_ids_to_ping = []
+
+    # remove the 'quote' blocks
+    original_post_contents.gsub!(%r{\[quote.*?\][^\[]+\[/quote\]}, '')
+    modified_post_contents.gsub!(%r{\[quote.*?\][^\[]+\[/quote\]}, '')
+    # begin
+      original_matches = original_post_contents.downcase.scan(/(@#{bot_username.downcase}) ([\S]+) ([\S]+)/)
+      modified_matches = modified_post_contents.downcase.scan(/(@#{bot_username.downcase}) ([\S]+) ([\S]+)/)
+
+      puts "original: " + original_matches.to_json, "modified: " + modified_matches.to_json
+
+      matches_diff = modified_matches.difference(original_matches)
+
+      puts "diff: " + matches_diff.to_json
+
+      # Return when we have the same commands as the original post or we don't have any commands in the edit
+      if !matches_diff.any? || modified_matches.empty?
+        return
+      end
+
+
+      modified_matches.each do |match|
+        command, category, size = match
+        if categories_to_fields.key?(category) && size
+          user_ids_for_category_and_size = user_ids_by_category_and_size(categories_to_fields[category], size)
+          user_ids_for_category_and_size.each do |user_id_for_category_and_size|
+            if User.find(user_id_for_category_and_size).custom_fields["receive_good_deals"]
+              user_ids_to_ping.push(user_id_for_category_and_size)
+            end
+          end
+        end
+      end
+      grouped_user_id_notifications_count = user_ids_to_ping.group_by{ |id| id.to_s }.transform_values{ |values| values.count }
+      grouped_user_id_notifications_count.each do |user_id, count|
+        Notification.create!(
+          notification_type: Notification.types[:custom],
+          user_id: user_id,
+          topic_id: original_post.topic_changes.topic["id"],
+          post_number: modified_post_contents.post_number,
+          data: {topic_title: original_post.topic_changes.topic["title"], icon: "mentioned", message: "js.clothing_deals.deal_notification_message", count: count}.to_json
+        )
+      end
+    # rescue => exception
+    #   return
+    # end
+  end
+
+  DiscourseEvent.on(:post_created) do |post|
     on_post_created(post)
+  end
+
+  DiscourseEvent.on(:post_edited) do |*params|
+    post, opts, post_revisor = params
+    puts "Revisor" + post_revisor.inspect
+    puts post.raw, post_revisor.cached_original_raw
+    # If it's not been edited during the grace period
+    if post_revisor.post_revision
+      puts "Revision" + post_revisor.post_revision.inspect
+      puts "Modifications" + post_revisor.post_revision.modifications.inspect
+      on_post_edited(post_revisor.post_revision.modifications["raw"][0], post_revisor.post_revision.modifications["raw"][1])
+    else
+      on_post_edited(post_revisor, post)
+    end
   end
 end
